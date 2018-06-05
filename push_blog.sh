21181b6bdf42020c5fcd634cd5d69faa503fb4e0
wz74666291 <847707695@qq.com>
2018-06-05 20:32:10 +0800

    update

--- push_blog.sh
+++ push_blog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git add .
-$current_date_time=`date "+%Y-%m-%d %H:%M:%S"`
+current_date_time=$(`date "+%Y-%m-%d %H:%M:%S"`)
 git commit -m "update $current_date_time"
 git push origin source
 hexo d -g
